53e37f3000e661c6173994fae25f2f624f04c89e
Shreyance Bodh <shreyancebodh2013@gmail.com>
2024-01-20 18:26:35 +0530

    Lec-10(introToJs, responsive-navbar)

---
+++ Lec-10/introToJS/strings.js
@@ -0,0 +1,12 @@
+
+var str1 = 'HELLO';
+var str2 = 'World!';
+
+var newStr = str1.toLocaleLowerCase();
+console.log(str1, str2.toLocaleUpperCase());
+console.log(newStr);
+
+// console.log(str1 + str2);
+
+// string methods w3school
+
